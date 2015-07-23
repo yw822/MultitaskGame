4c7d78b8f10350fa55dec06bdda19f354cd4a189
SimoPrG <simeon_petev@hotmail.com>
2015-07-23 17:20:17 +0300

    refactor application.js

--- scripts/application.js
+++ scripts/application.js
@@ -1,23 +1,30 @@
-var gameManager = (function () {
-    var gameManager = {},
-        gameOver = false,
+(function () {
+    var gameOver = false,
         game_1 = Object.create(game1).init(),
         game_2 = Object.create(game2).init(),
         game_3 = Object.create(game3).init(),
         game_4 = Object.create(game4).init(),
         games = [game_1, game_2, game_3, game_4];
 
-    (function animate() {
+    function updateGames() {
         games.forEach(function (game) {
             game.update();
         });
+    }
 
+    function checkGameOver() {
         gameOver = games.some(function (game) {
             return game.over;
         });
+    }
 
+    function animate() {
+        updateGames();
+        checkGameOver();
         if (!gameOver) {
             requestAnimationFrame(animate);
         }
-    }());
+    }
+
+    animate();
 }());
