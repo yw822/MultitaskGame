c978432c4bc107a6d16e0d9fa37752c05db363a6
SimoPrG <simeon_petev@hotmail.com>
2015-07-24 22:14:12 +0300

    Add properties xCoordinateA, yCoordinateA, xCoordinateB, yCoordinateB, xCoordinateC, yCoordinateC to triangle

--- scripts/triangle.js
+++ scripts/triangle.js
@@ -2,10 +2,73 @@ var triangle = (function (parent) {
     var triangle = Object.create(parent);
 
     Object.defineProperty(triangle, 'init', {
-        value: function (xCoordinate, yCoordinate, fill, stroke) {
-            parent.init.call(this, xCoordinate, yCoordinate, fill, stroke);
+        value: function (xCoordinateA, yCoordinateA, xCoordinateB, yCoordinateB, xCoordinateC, yCoordinateC, fill, stroke) {
+            parent.init.call(this, xCoordinateA, yCoordinateA, fill, stroke);
+            this.xCoordinateB = xCoordinateB;
+            this.yCoordinateB = yCoordinateB;
+            this.xCoordinateC = xCoordinateC;
+            this.yCoordinateC = yCoordinateC;
 
             return this;
+        }
+    });
+
+    // Triangle's xCoordinate and yCoordinate are the coordinates of its point A.
+    Object.defineProperty(triangle, 'xCoordinateA', {
+        get: function () {
+            return this.xCoordinate;
+        },
+        set: function (value) {
+            this.xCoordinate = value;
+        }
+    });
+
+    Object.defineProperty(triangle, 'yCoordinateA', {
+        get: function () {
+            return this.yCoordinate;
+        },
+        set: function (value) {
+            this.yCoordinate = value;
+        }
+    });
+
+    Object.defineProperty(triangle, 'xCoordinateB', {
+        get: function () {
+            return this._xCoordinateB;
+        },
+        set: function (value) {
+            validator.validateIfRealNumber(value, "xCoordinateB");
+            this._xCoordinateB = value;
+        }
+    });
+
+    Object.defineProperty(triangle, 'yCoordinateB', {
+        get: function () {
+            return this._yCoordinateB;
+        },
+        set: function (value) {
+            validator.validateIfRealNumber(value, "yCoordinateB");
+            this._yCoordinateB = value;
+        }
+    });
+
+    Object.defineProperty(triangle, 'xCoordinateC', {
+        get: function () {
+            return this._xCoordinateC;
+        },
+        set: function (value) {
+            validator.validateIfRealNumber(value, "xCoordinateC");
+            this._xCoordinateC = value;
+        }
+    });
+
+    Object.defineProperty(triangle, 'yCoordinateC', {
+        get: function () {
+            return this._yCoordinateC;
+        },
+        set: function (value) {
+            validator.validateIfRealNumber(value, "yCoordinateC");
+            this._yCoordinateC = value;
         }
     });
 
