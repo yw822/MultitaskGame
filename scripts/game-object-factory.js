e8afb22e3681d041d4ff8ed1690462dc92afe6c2
svstoichkov <sv.stoichkov@yahoo.com>
2015-07-25 17:08:33 +0300

    gameObjectFactory

---
+++ scripts/game-object-factory.js
@@ -0,0 +1,16 @@
+function gameObjectFactory(){
+    return {
+        getRectangle: function (xCoordinate, yCoordinate, width, height, fill, stroke, strokeWidth) {
+            return Object.create(rectangle).init(xCoordinate, yCoordinate, width, height, fill, stroke, strokeWidth)
+        },
+        getRectangleWithText: function (xCoordinate, yCoordinate, width, height, fill, stroke, strokeWidth, text) {
+            return Object.create(rectangleWithText).init(xCoordinate, yCoordinate, width, height, fill, stroke, strokeWidth, text)
+        },
+        getTriangle: function (xCoordinateA, yCoordinateA, xCoordinateB, yCoordinateB, xCoordinateC, yCoordinateC, fill, stroke, strokeWidth) {
+            return Object.create(triangle).init(xCoordinateA, yCoordinateA, xCoordinateB, yCoordinateB, xCoordinateC, yCoordinateC, fill, stroke, strokeWidth)
+        },
+        getCircle: function (xCoordinate, yCoordinate, radius, fill, stroke, strokeWidth) {
+            return Object.create(triangle).init(xCoordinate, yCoordinate, radius, fill, stroke, strokeWidth)
+        }
+    };
+}
