31e390c0f897d7c06252ed442573a3355dd5bf39
SimoPrG <simeon_petev@hotmail.com>
2015-07-25 12:47:53 +0300

    Add rectangle-with-text.js script

---
+++ scripts/rectangle-with-text.js
@@ -0,0 +1,24 @@
+var rectangleWithText = (function(parent) {
+    var rectangleWithText = Object.create(parent);
+
+    Object.defineProperty(rectangleWithText, 'init', {
+        value: function (xCoordinate, yCoordinate, width, height, fill, stroke, strokeWidth, text) {
+            parent.init.call(this, xCoordinate, yCoordinate, width, height, fill, stroke, strokeWidth);
+            this.text = text;
+
+            return this;
+        }
+    });
+
+    Object.defineProperty(rectangleWithText, 'text', {
+        get: function () {
+            return this._text;
+        },
+        set: function (value) {
+            validator.validateIfString(value, 'text');
+            this._text = value;
+        }
+    });
+
+    return rectangleWithText;
+}(rectangle));
