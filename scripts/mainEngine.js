8ffc69ebb5b15eb3c4ca97f7028f69e015251c31
TsvetanRazsolkov <tsvetan.razsolkov@gmail.com>
2015-07-28 17:28:33 +0300

    mainEngine.js small correction.

---
+++ scripts/mainEngine.js
@@ -0,0 +1,34 @@
+﻿module.exports = (function () {    
+    var engine = {},
+        games,
+        gameOver = false;
+    
+    function updateGames() {
+        games.forEach(function (game) {
+            game.update();
+        });
+    }
+
+    function checkGameOver() {
+        gameOver = games.some(function (game) {
+            return game.over;
+        });
+    }
+
+    function animate() {
+        updateGames();
+        checkGameOver();
+        if (!gameOver) {
+            requestAnimationFrame(animate);
+        }
+    }
+
+    Object.defineProperty(engine, 'runGames', {
+        value: function (gamesList) {
+            games = gamesList
+            animate();
+        }
+    });
+
+    return engine;
+}());
