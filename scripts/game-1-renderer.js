fc9059a7729343f8fe1bb8eb0a39c559aa776a4f
boytchoteofilov <boytcho.teofilov@gmail.com>
2015-07-30 00:47:57 +0300

    game 1 renderer file

--- scripts/game-1-renderer.js
+++ scripts/game-1-renderer.js
@@ -1,22 +1,50 @@
 module.exports = (function (parent) {
-    var game1Renderer = Object.create(parent);
-    // Consider declaring here a private variable to hold your Canvas Context or SVG element.
+    var game1Renderer = Object.create(parent),
+        constants = require('./constants.js'),
+        circle = require('./circle.js'),
+        stage = new Kinetic.Stage({
+            container: document.getElementById('game-1'),
+            width: constants.CANVAS_WIDTH,
+            height: constants.CANVAS_HEIGHT
+        }),
+        layer = new Kinetic.Layer({ width: 300, height: 200});
 
     Object.defineProperty(game1Renderer, 'clearStage', {
         value: function () {
-            //TODO: Implement this method to clear the Canvas. If you are using SVG, you can leave this method empty. Your choice :)
-
-            //Delete this line!
-            parent.clearStage.call(this);
+            layer.removeChildren();
         }
     });
 
     Object.defineProperty(game1Renderer, 'render', {
-        value: function (gameObject) {
-            //TODO: Implement this method to render the game objects.
+        value: function (gameObject, rotationAngle) {
+            var figure;
+            if (circle.isPrototypeOf(gameObject)) {
+                figure = new Kinetic.Circle({
+                    x: gameObject.xCoordinate,
+                    y: gameObject.yCoordinate,
+                    radius: gameObject.radius,
+                    fill: gameObject.fill,
+                    stroke: gameObject.stroke,
+                    strokeWidth: gameObject.strokeWidth
+                });
+            }
+            else {
+                figure = new Kinetic.Line({
+                    points: gameObject.getCoordinatesAsArray(),
+                    stroke: gameObject.stroke,
+                    fill: gameObject.fill,
+                    strokeWidth: gameObject.strokeWidth,
+                    closed: true
+                });
+            }           
 
-            //Delete this line!
-            parent.render.call(this, gameObject);
+            layer.add(figure);
+            var w = layer.getWidth(),
+                h = layer.getHeight();
+            layer.setOffset({x:w / 2,y:h / 2});
+            layer.setPosition({ x: w / 2, y: h / 2 });
+            layer.rotateDeg(rotationAngle);
+            stage.add(layer);
         }
     });
 
